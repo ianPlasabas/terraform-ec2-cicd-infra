6435269cadd84022e2a4271ef89ee29ea1d8ba11
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-10-26 10:19:21 +0800

    Modified main.tf for initial deployment

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-ec2-cicd-infra-876862731491"
+    key    = "ec2/terraform.state"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "server_01" {
+  ami           = "ami-03bec25d3c8e6cd26"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "server-01"
+  }
+}
+
